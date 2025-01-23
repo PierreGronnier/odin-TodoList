4b98923f699230a5a107f2379853ac83ef7ae254
Pierre Gronnier <pierre.gronnier@gmail.com>
2025-01-23 15:53:30 +0100

    add task.js

---
+++ src/task.js
@@ -0,0 +1,15 @@
+export default class Task {
+    constructor(title, description, dueDate, priority, notes = "", checklist = []) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checklist = checklist;
+        this.completed = false; 
+    }
+
+    toggleComplete() {
+        this.completed = !this.completed;
+    }
+}
